a7a4bf53fc6db2f4e0da81d0d312f3250a96f3e5
641i130 <galileoray@gmail.com>
2021-09-11 14:18:32 -0500

    Added lily58

--- lily58/keymaps/caret/keymap.c
+++ lily58/keymaps/caret/keymap.c
@@ -6,11 +6,16 @@ enum layer_number {
   _QWERTY = 0,
   _DVORAK,
   _LOWER,
-  _RAISE
+  _RAISE,
+};
+const uint16_t PROGMEM test_combo1[] = {KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM test_combo2[] = {KC_F, KC_G, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(test_combo1, KC_PGUP),
+    COMBO(test_combo2, KC_PGDOWN), // keycodes with modifiers are possible too!
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |   1  |   2  |   3  |   4  |   5  |   6  |                    |   7  |   8  |   9  |   0  |   (  |  )   |
@@ -30,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_1,     KC_2,   KC_3,    KC_4,    KC_5,    KC_6,                     KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC,\
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_GRV,  LT(_KC_F13,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_PIPE, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_GRV,  LT(_LOWER,KC_F13),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_PIPE, \
                         LT(_LOWER,KC_LALT), KC_LGUI, KC_LCTRL, KC_SPC, KC_ENT, LT(_RAISE,KC_BSPC), MO(_RAISE), MO(_LOWER) \
 ),
 /* LOWER
@@ -72,8 +77,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______,  KC_END, XXXXXXX, KC_PGDOWN,  KC_PGUP, KC_RBRC, KC_BSLS, \
+  _______, _______, KC_MEDIA_PREV_TRACK,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,KC_RSFT, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
+  _______, _______, KC__MUTE,KC__VOLDOWN,KC__VOLUP, _______,   _______, _______,  KC_END, XXXXXXX, KC_PGDOWN,  KC_PGUP, KC_RBRC, KC_BSLS, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
 /* DVORAK
@@ -294,4 +299,4 @@ void oled_task_user(void) {
         //oled_write(wpm_str, false);
     }
 }
-}
+
