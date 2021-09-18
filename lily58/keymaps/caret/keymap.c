045a8b45f834fa5f263fb3fea3a5e1bcd8c1926c
641i130 <galileoray@gmail.com>
2021-09-18 14:37:51 -0500

    Added lily58

--- lily58/keymaps/caret/keymap.c
+++ lily58/keymaps/caret/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   `   |    | Layer?|------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  |   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LCTRL | /Space  /       \ENTER \  |BAKSP |RAISE | LOWER|
+ *                   | LAlt | LGUI |LCTRL | /Space  /       \ENTER \  |BK/RA |RAISE | LOWER|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,\
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
-  _______, KC_QUES, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______, KC_QUES, _______, _______, _______, _______, DF(_DVORAK), DF(_QWERTY), _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 /* RAISE 
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-  _______, _______, KC_MEDIA_PREV_TRACK,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,KC_RSFT, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
+  _______, _______, KC_MPRV,KC_MPLY,KC_MNXT,KC_RSFT, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
   _______, _______, KC__MUTE,KC__VOLDOWN,KC__VOLUP, _______,   _______, _______,  KC_END, XXXXXXX, KC_PGDOWN,  KC_PGUP, KC_RBRC, KC_BSLS, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
@@ -98,10 +98,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_DVORAK] = LAYOUT( \
   KC_1,     KC_2,   KC_3,    KC_4,    KC_5,    KC_6,                     KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC,\
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  KC_ESC,KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_GRV,  KC_F13,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_PIPE, \
-                        KC_LALT, KC_LGUI, KC_LCTRL, KC_SPC, KC_ENT, LT(_RAISE,KC_BSPC), MO(_RAISE), KC_NO \
+  KC_TAB,   KC_QUOT,KC_COMM, KC_DOT,  KC_P,    KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLASH, \
+  KC_ESC,   KC_A,   KC_O,    KC_E,    KC_U,    KC_I,                     KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINUS, \
+  KC_LSFT, KC_SCOLON,KC_Q,   KC_J,    KC_K,    KC_X, KC_GRV,LT(_LOWER,KC_F13),    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,   KC_BSLASH, \
+              LT(_LOWER,KC_LALT), KC_LGUI, KC_LCTRL, KC_SPC, KC_ENT, LT(_RAISE,KC_BSPC), MO(_RAISE), MO(_LOWER) \
 )
 
 };
